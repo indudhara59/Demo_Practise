1a9adb5878d1be74139375daa2a1cd924b15e43d
Indudhara S <indudhara59@gmail.com>
2026-04-20 13:22:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ if age < 18:
     print("You are a minor.")
 elif age >= 18 and age < 65:
     print("You are an adult.")
-else:    print("You are a senior.")
+else:    
+    print("You are a senior.")
