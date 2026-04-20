23420a6f1bfa44268092706bf81822baceb3e1f2
Indudhara S <indudhara59@gmail.com>
2026-04-20 12:49:17 +0200

    Updated main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 print("Hello World!")
 name = str(input("Enter your name: "))
 print(f"Hello {name}!, \nHow are you doing today?")
+age = int(input("Enter your age: "))
+if age < 18:
+    print("You are a minor.")
+elif age >= 18 and age < 65:
+    print("You are an adult.")
+else:    print("You are a senior.")
